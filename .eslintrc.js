03d007dae561350d70ff683f72a8ec42a02fe072
hxy1997 <huxingyi1997@gmail.com>
2020-09-16 12:07:20 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,13 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    'space-before-function-paren': 0,
+    'generator-star-spacing': 0,
+    'no-multiple-empty-lines': 0,
+    'quote-props': [0, 'always'],
+    semi: 0,
+    indent: ['off', 2],
+    'no-tabs': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,16 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    // 导入弹框提示组件
+    Message
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 进行全局挂载：
+Vue.prototype.$message = Message
