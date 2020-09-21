5df3233c402b1d75b6aa9566ee83404bb62e8794
hxy1997 <huxingyi1997@gmail.com>
2020-09-21 16:14:23 +0800

    完成了分类参数开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -29,7 +29,10 @@ import {
     Tree,
     Select,
     Option,
-    Cascader
+    Cascader,
+    Alert,
+    Tabs,
+    TabPane
 } from 'element-ui'
 
 Vue.use(Button)
@@ -63,3 +66,6 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
