91320810fc247b2de7ac18f53902aca9c64a0d2c
hw_18114021181 <huxingyi@depthsdata.com>
2021-03-07 19:43:18 +0800

    修复图标无法显示

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -18,7 +18,7 @@
     </el-card>
 
     <!-- 角色列表区域 -->
-    <el-table :data="rolelist" border stripe>
+    <el-table :data="rolelist" border stripe row-key="id">
       <!-- 展开列 -->
       <el-table-column type="expand">
         <template slot-scope="scope">
